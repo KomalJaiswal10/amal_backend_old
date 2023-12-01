efdacf2faa3fe272fed34b5f4c54e50a255fdebf
Komal Jaiswal <komaljaiswal2810@gmail.com>
2023-12-01 23:04:50 +0530

    basic ts setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Application, } from "express";
+import "dotenv/config"
+import bodyParser from "body-parser";
+
+const app: Application = express();
+const port: string = process.env.PORT || '8000';
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.listen(port, () => console.log(`server started at port ${port}`))
